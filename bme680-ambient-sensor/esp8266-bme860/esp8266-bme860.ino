281dece3e44059071b78ffa0c53734d535992462
dperezmavro <dio.pm@live.com>
2025-07-20 17:06:57 +0100

    added wifi capability

--- bme680-ambient-sensor/esp8266-bme860/esp8266-bme860.ino
+++ bme680-ambient-sensor/esp8266-bme860/esp8266-bme860.ino
@@ -1,9 +1,16 @@
-#include "Adafruit_BME680.h"
+#include <Adafruit_BME680.h>
+#include <ESP8266WiFi.h>
 #include <Adafruit_Sensor.h>
 #include <ArduinoJson.h>
 #include <Wire.h>
+#include "./secrets.h"
 
 #define SEALEVELPRESSURE_HPA (1013.25)
+#define SAMPLE_INTERVAL_MS 5000
+#define OUTPUT_BUFFER_SIZE 256
+
+const char* wifi_ssid = WIFI_SSID;
+const char* wifi_pass = WIFI_PASS;
 
 Adafruit_BME680 bme(&Wire);  // I2C
 
@@ -19,7 +26,8 @@ void setup() {
   Serial.begin(9600);
   while (!Serial)
     ;
-  Serial.println(F("BME680 test"));
+  Serial.println();
+  Serial.println(F("ESP8266 + BME680 starting up"));
 
   if (!bme.begin()) {
     Serial.println("Could not find a valid BME680 sensor, check wiring!");
@@ -27,12 +35,26 @@ void setup() {
       ;
   }
 
+  Serial.println(F("[*] Starting WiFi setup"));
+  WiFi.begin(wifi_ssid, wifi_pass);
+
+  Serial.println("[*] Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.printf("  last status: %d.\n", WiFi.status());
+  }
+  Serial.printf("[+] Connected, IP address: %s\n", WiFi.localIP().toString().c_str());
+  Serial.println(F("[+] WiFi setup successful!"));
+
+
+  Serial.println(F("[*] Starting BME sensor setup"));
   // Set up oversampling and filter initialization
   bme.setTemperatureOversampling(BME680_OS_8X);
   bme.setHumidityOversampling(BME680_OS_2X);
   bme.setPressureOversampling(BME680_OS_4X);
   bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
   bme.setGasHeater(320, 150);  // 320*C for 150 ms
+  Serial.println(F("[+] BME sensor setup successful!"));
 }
 
 
@@ -52,9 +74,9 @@ void loop() {
   doc["pressure (hPa)"] = bme.pressure;
   doc["temperature (*C))"] = bme.temperature;
 
-  char output[256];
+  char output[OUTPUT_BUFFER_SIZE];
 
   serializeJson(doc, output, sizeof(output));
   Serial.println(output);
-  delay(2000);
+  delay(SAMPLE_INTERVAL_MS);
 }
---
+++ bme680-ambient-sensor/esp8266-bme860/secrets_template.h
@@ -0,0 +1,7 @@
+#ifndef SECRETS
+#define SECRETS
+
+#define WIFI_SSID "DEFINE_ME"
+#define WIFI_PASS "DEFINE_ME"
+
+#endif
