610fbc6daeefc1699edbcab61d72bfaa219e6ee3
Dionisio Perez Mavrogenis <dperezmavro@users.noreply.github.com>
2025-07-26 19:30:16 +0100

    added influxdb client (#2)
    * added influxdb client
    
    * cleanup
    
    ---------
    
    Co-authored-by: Dionisio Perez Mavrogenis <dio@macrocosmos.ai>

--- bme680-ambient-sensor/esp8266-bme860/esp8266-bme860.ino
+++ bme680-ambient-sensor/esp8266-bme860/esp8266-bme860.ino
@@ -1,5 +1,5 @@
-#include "./aws_iot_client.h"
-
+// #include "./aws_iot_client.h"
+#include "./influx_db.h"
 
 #ifndef WIFI_SETTINGS_H
 #define WIFI_SETTINGS_H
@@ -19,7 +19,6 @@ const char* wifi_pass = WIFI_PASS;
 #include <ArduinoJson.h>
 #include <Wire.h>
 
-
 #define SEALEVELPRESSURE_HPA (1013.25)
 #define SAMPLE_INTERVAL_MS 5000
 #define OUTPUT_BUFFER_SIZE 256
@@ -35,28 +34,17 @@ float dewPont;
 JsonDocument doc;
 #endif  // SENSOR_SETTINGS_H
 
-// MQTT settings
-AWSIotClient *client;
-// MQTT topic to publish sensor data
+#define DEVICE "bme680"
 
-void setup_aws() {
-  Serial.println("[*] Connecting to AWS");
-  
-  client = new AWSIotClient();
-  
-  client->connect();
-  
-  Serial.println("[+] AWS IoT Connected!");
-}
+InfluxDB influx_db(DEVICE);
 
 
 void setup_sensors() {
   Serial.println(F("[*] Starting BME sensor setup"));
 
-  if (!bme.begin()) {
+  while (!bme.begin()) {
     Serial.println("Could not find a valid BME680 sensor, check wiring!");
-    while (1)
-      ;
+    delay(1500);
   }
   // Set up oversampling and filter initialization
   bme.setTemperatureOversampling(BME680_OS_8X);
@@ -64,6 +52,7 @@ void setup_sensors() {
   bme.setPressureOversampling(BME680_OS_4X);
   bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
   bme.setGasHeater(320, 150);  // 320*C for 150 ms
+
   Serial.println(F("[+] BME sensor setup successful!"));
 }
 
@@ -73,47 +62,63 @@ void setup_wifi() {
 
   Serial.println("[*] Connecting");
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
+    delay(1500);
     Serial.printf("  last status: %d.\n", WiFi.status());
   }
   Serial.printf("[+] Connected, IP address: %s\n", WiFi.localIP().toString().c_str());
   Serial.println(F("[+] WiFi setup successful!"));
 }
 
+void setup_ntp() {
+  Serial.println(F("[*] Starting NTP setup"));
+  timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
+  Serial.println(F("[+] NTP setup successful!"));
+}
+
 void setup() {
   Serial.begin(9600);
-  while (!Serial)
-    ;
+  while (!Serial) {
+    delay(1000);
+  }
+
   Serial.println();
   Serial.println(F("ESP8266 + BME680 starting up"));
-
+  
+  
   setup_wifi();
+  setup_ntp();
   setup_sensors();
-  setup_aws();
+  
+  if (!influx_db.setup()) {
+    Serial.println(F("InfluxDB setup failed"));
+    return;
+  }
 
   Serial.println(F("[+] Setup completed successfully!"));
 }
 
 void loop() {
+  
   if (!bme.performReading()) {
     Serial.println("Failed to perform reading :(");
+    delay(1000);
     return;
   }
 
-  doc["sensor"] = "bme680";
-  doc["location"] = "living_room";
-  doc["altitude (m)"] = bme.readAltitude(SEALEVELPRESSURE_HPA);
-  doc["gas (KOhms)"] = bme.gas_resistance / 1000.0;
-  doc["humidity (%)"] = bme.humidity;
-  doc["pressure (hPa)"] = bme.pressure;
-  doc["temperature (*C))"] = bme.temperature;
 
-  char jsonBuffer[OUTPUT_BUFFER_SIZE];
+  std::map<std::string, float> values;
+  values["temperature (C)"] = bme.temperature;
+  values["humidity (%)"] = bme.humidity;
+  values["pressure (hPa)"] = bme.pressure;
+  values["gas (KOhms)"] = bme.gas_resistance / 1000.0;
+  values["altitude (m)"] = bme.readAltitude(SEALEVELPRESSURE_HPA);
 
-  serializeJson(doc, jsonBuffer, sizeof(jsonBuffer));
-  Serial.println(jsonBuffer);
+  // values["location"] = "living_room";
+  // values["sensor_device"] = DEVICE;
+  
 
-  client->publish_message(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
-  client->loop();
+  influx_db.write_point(values);
+  Serial.println("Waiting 1 second");
+
   delay(SAMPLE_INTERVAL_MS);
 }
---
+++ bme680-ambient-sensor/esp8266-bme860/influx_db.h
@@ -0,0 +1,30 @@
+#ifndef _INFLUX_DB_H
+#define _INFLUX_DB_H
+
+
+#include <InfluxDbClient.h>
+#include <InfluxDbCloud.h>
+#include <map>
+#include <string>
+
+#include "influx_db_secrets.h"
+
+#define INFLUXDB_BUCKET "bme680_sensor"
+
+// Time zone info
+#define TZ_INFO "UTC0"
+
+class InfluxDB {
+  private:
+    InfluxDBClient *influx_client;
+    Point *sensor;
+  public:
+    InfluxDB(const char* point_name);
+    bool write_point(
+        const std::map<std::string, float>& values
+    );
+    bool setup();
+};
+
+
+#endif
